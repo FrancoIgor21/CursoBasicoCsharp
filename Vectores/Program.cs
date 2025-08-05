8a6bd510cc31d45aebee7dbd2b93388f35cd2d3b
FrancoIgor21 <igorfrancob19@gmail.com>
2025-08-05 16:27:15 -0300

    Add files via upload

---
+++ Vectores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Nombres = { "Igor", "Carlos", "Pinga", "Joseluis" };
+
+            Console.Write("Elija un vector del 0 al 3: ");
+            Console.ReadLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While
+{
+    internal class program1
+    {
+        static void Main(string[] args)
+        {
+            int Correlativo = 0;
+
+            while (Correlativo<=12)
+            {
+                Console.WriteLine("5 x "+Correlativo+" = "+(5*Correlativo));
+                Correlativo++;
+            }
+           
+            Console.ReadKey();
+        }
+
+    }
+
+}
