4c35f5090b3102a4ced18803f2a90f07b6e78c27
FrancoIgor21 <igorfrancob19@gmail.com>
2025-08-05 16:23:17 -0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            string Nombre;
+            double Sueldo;
+
+            Console.Write("Ingrese Su Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese Su Sueldo: ");
+            Sueldo = Convert.ToDouble(Console.ReadLine());
+
+            if (Sueldo <= 1024)
+            {
+                Console.WriteLine(Nombre + " El sueldo que tenes es el minimo ");
+
+            }
+            else if (Sueldo >1024 && Sueldo <= 2000)
+            {
+                Console.WriteLine(Nombre + " El sueldo que tenes es rentable ");
+            }
+            else if (Sueldo > 2000)
+            {
+                Console.WriteLine(Nombre + " El sueldo que tenes es generoso ");
+            }
+
+
+            
+           
+       
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Do While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoWhile
+
+{
+    internal class DoWhile
+    {
+        static void Main(string[] args)
+        {
+
+        int Numero;
+        Numero = 0;
+
+        do
+        {
+            Console.WriteLine("cargando "+ Numero + "%");
+                Numero++;
+
+        } while (Numero<=1);
+        
+        Console.ReadKey();
+        }
+    }
+}
